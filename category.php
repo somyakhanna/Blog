3a11784bbd3bfc57616e50170f992488fb1aebdf
somyakhanna <30292668+somyakhanna@users.noreply.github.com>
2017-08-01 21:22:04 +0530

    First

---
+++ category.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Plan of Action</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+           <link href="css/blog.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+  <div id="category">
+    <div id="header">
+        <?php include("menu.html");?>
+    <br>
+    <br>
+    <!--Banner Ends-->
+    <div class="container" id="content">
+        
+         <br>
+         <h1>Food And Lifestyle</h1>
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/street.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Food and Lifestyle</a></span></div><br>
+          <div class='f2'><span><a>Street Food at its Best</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        
+         <h1>Art,Culture And Heritage</h1>
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/cp.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Art,Culture And Heritage</a></span></div><br>
+          <div class='f2'><span><a>Title Here!</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        <h1>Fashion And Beauty</h1>
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/poa.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Fashion And Beauty</a></span></div><br>
+          <div class='f2'><span><a>Title Here!</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        
+            <h1>Entertainment</h1>
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/2.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Entertainment</a></span></div><br>
+          <div class='f2'><span><a>Title Here!</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        
+       
+               
+            <h1 >Health And Wellness</h1>
+           
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/fbanner.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Health And Wellness</a></span></div><br>
+          <div class='f2'><span><a>Title Here!</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        <h1>Travel And Adventure</h1>
+            <?php 
+            $i=0;
+            while ($i<3)
+            { echo "
+            
+    <div class='pro_part col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+      <div class='h1' id='try' style='background-image: url(img/4.jpg);
+background-position: center;background-size:cover;'><a href='#'></a>
+<div class='h2'>
+          <div class='fl'><span><a>Travel And Adventure</a></span></div><br>
+          <div class='f2'><span><a>Title Here!</a></span></div><br>
+          <div class='f3'><span><a>Anuj Garg</a></span></div>
+      </div>
+       
+        <div class='clear'></div>
+      </div>
+    </div>
+";
+            $i++;
+}
+
+            ?>
+        
+        <div class="row load">
+            <div class="col-xs-12 col-sm-12 col-lg-4 col-md-4 col-lg-offset-5 col-md-offset-5"> <a href="#" class="btn btn-lg btn-success">LOAD MORE</a></div></div>
+        
+                 
+        <div class="row">
+            <div id="left-wrapper" class="col-xs-12 col-sm-12 col-lg-8 col-md-8">
+
+                <section id="right">
+
+                    <div class="mini-posts">
+
+                        <h2 id="heading">Browse Our Other Genres</h2><br/>
+
+                        
+                        <!-- Mini Post -->
+
+
+                        <article class="mini-post col-xs-12 col-sm-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Lifestyle</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/1.jpg" alt="" /></a>
+                        </article>
+
+
+
+                        <article class="mini-post col-xs-12 col-sm-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Entertainment</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/event.jpg" alt="" /></a>
+                        </article>
+
+
+
+                        <article class="mini-post col-xs-12 col-sm-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Art,Culture &amp; Heritage</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/3.jpg" alt="" /></a>
+                        </article>
+
+                        <article class="mini-post col-xs-12 col-sm-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Health &amp; Wellness</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/food.jpg" alt="" /></a>
+                        </article>
+
+
+
+                        <article class="mini-post col-xs-12 col-sm-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Fashion &amp; Beauty</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/fashion.jpg" alt="" /></a>
+                        </article>
+
+                        <article class="mini-post col-sm-12 col-xs-12 col-lg-6 col-md-6">
+
+                            <header>
+                                <h3><a href="" id="genre">Travel &amp; Adventure</a></h3>
+                            </header>
+                            <a href="" class="image"><img src="img/travel.jpg" alt="" /></a>
+                        </article>
+
+                    </div>
+                </section>
+
+
+            </div>
+            <div id="right-wrapper" class="col-xs-12 col-sm-12 col-lg-4 col-md-4">
+
+                <!-- Posts List -->
+                <section id="right" class="row">
+                    <div>
+                        <br>
+                        <ul class="posts col-xs-12 col-sm-12 col-lg-12 col-md-12">
+
+                            <h1 id="heading">Recommended for you</h1>
+
+                            <?php 
+                            $p=0;
+                            while($p<4)
+                            {
+                                echo"<li>
+										<article>
+											<header>
+												<h3><a href='#' id='genre'>Be the Best Version of you</a></h3>
+												<p>By: Prabhat Khanna</p>
+												<time class='published' datetime='2017-08-2'>August 2, 2017</time>
+											</header>
+											<a href='#' class='image'><img src='img/poa.jpg' alt='' /></a>
+										</article>
+									</li>";
+                                $p++;
+                            }
+                        ?>
+
+                        </ul>
+                    </div>
+                </section>
+
+
+            </div>
+        </div>
+    </div>
+
+
+         <?php include("footer.html");?>
+                    </div>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
---
+++ post.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Plan of Action</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog2.css" rel="stylesheet">
+    
+
+  
+
+</head>
+
+<body>
+<?php include("menu.html");?>
+    <!-- Navigation -->
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                    <div class="post-heading">
+                        <h1>This Bhayankar Thandi Coffee In Gurugram Is A Liquid Hug For Your Brain!</h1>
+                        <a href="#"><h5 class="category">FOOD AND LIFESTYLE</h5></a>
+                        <span class="meta">Posted by <a href="#">Anuj Garg</a> on August 2, 2017</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                                <div id="share-buttons">
+    
+ 
+    <!-- Facebook -->
+    <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" id="fb" target="_blank">
+        <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />SHARE ON FACEBOOK
+    </a>
+<!-- Twitter -->
+    <a id="twi" href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+        <img src="https://simplesharebuttons.com/images/somacro/twitter.png"  alt="Twitter" />SHARE ON TWITTER
+</a>
+    </div>
+                    <p>To the point: Mogli’s at City Centre, Gurugram is the perfect place to get a coffee dose, for kick-starting your day</p>
+                    <p>Two guys were sitting idle trying to figure out where to get a cup of nice coffee. An idea came to their caffeine deprived brains-why not make one on their own. This is how they decided to take up a noble cause of selling brilliant coffee, on a price which is cheaper than the popular coffee shops. </p>
+                    <a href="#"> 
+                    <img class="img-responsive col-xs-12 col-md-12 col-lg-12" src="img/story1.jpg" alt="">
+                    </a>
+                    <span class="caption text-muted">Some Text About The Image Here</span>
+                    <p>It is a blessing for those who cannot stay without coffee, but don’t want to spend too much on it. That is why they have a tagline which says “It just tastes expensive�?. What made us fall in love with this place is their, Bhayankar Thandi Coffee. Don’t judge it by the name! But it is indeed as kickass as the name.</p>
+
+                    <p>Whoever said there’s no better high than a coffee high, probably had this coffee before saying it. Strong brewed coffee, with flavours like Brownie, Choco Chip to name a few. But our favourite is the Bhayankar Cramel Cold Coffee. We bet you’ll get addicted to it. “Isko piya toh life jhingalala!�?</p>
+                    
+                    <h2 class="section-heading">The Cold Coffee</h2>
+                    <p>Whoever said there’s no better high than a coffee high, probably had this coffee before saying it. Strong brewed coffee, with flavours like Brownie, Choco Chip to name a few. But our favourite is the Bhayankar Cramel Cold Coffee. We bet you’ll get addicted to it. “Isko piya toh life jhingalala!�?</p>
+                    <a href="#"> 
+                     <img class="img-responsive col-xs-12 col-md-12 col-lg-12" src="img/story2.jpg" alt="">
+                    </a>
+                    <span class="caption text-muted">Some Text About The Image Here</span>
+                    <p>It is a blessing for those who cannot stay without coffee, but don’t want to spend too much on it. That is why they have a tagline which says “It just tastes expensive�?. What made us fall in love with this place is their, Bhayankar Thandi Coffee. Don’t judge it by the name! But it is indeed as kickass as the name.</p>
+
+                    <blockquote class="blockquote">Coffee is a language in itself!</blockquote>
+
+                    <p>It is a blessing for those who cannot stay without coffee, but don’t want to spend too much on it. That is why they have a tagline which says “It just tastes expensive�?. What made us fall in love with this place is their, Bhayankar Thandi Coffee. Don’t judge it by the name! But it is indeed as kickass as the name.</p>
+
+                    <h2 class="section-heading">To all the Coffee Lovers</h2>
+
+                    <p>It is a blessing for those who cannot stay without coffee, but don’t want to spend too much on it. That is why they have a tagline which says “It just tastes expensive�?. What made us fall in love with this place is their, Bhayankar Thandi Coffee. Don’t judge it by the name! But it is indeed as kickass as the name.</p>
+
+                    <a href="#">
+                         <img class="img-responsive col-xs-12 col-md-12 col-lg-12" src="img/story3.jpg" alt="">
+                    </a>
+                    <span class="caption text-muted">Some Text About The Image Here</span>
+
+                    <p>It is a blessing for those who cannot stay without coffee, but don’t want to spend too much on it. That is why they have a tagline which says “It just tastes expensive�?. What made us fall in love with this place is their, Bhayankar Thandi Coffee. Don’t judge it by the name! But it is indeed as kickass as the name.</p>
+
+                </div>
+            </div>
+        </div>
+    </article>
+
+    <hr>
+    
+    <!-- Blog Comments -->
+<div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+
+                <!-- Comments Form -->
+                <div class="well">
+                    <h4>Leave a Comment:</h4>
+                    <form role="form">
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+                <hr>
+
+                <!-- Posted Comments -->
+
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">NAME HERE
+                            <small>August 25, 2017 at 1:30 PM</small>
+                        </h4>
+                    Great Place. 
+                    </div>
+                </div>
+
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">NAME HERE
+                            <small>August 25, 2017 at 5:00 PM</small>
+                        </h4>
+                        Nice Blog! 
+                    </div>
+                </div>
+
+            </div>
+
+            </div>
+</div>
+    <?php include("footer.html");?>
+    
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
